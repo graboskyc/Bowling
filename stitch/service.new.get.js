fc4bb1047aae2014112f0307d1dbd412c35cc43e
Chris Grabosky <chris@grabosky.net>
2018-10-05 13:36:10 -0400

    backing up stitch http function

---
+++ stitch/service.new.get.js
@@ -0,0 +1,16 @@
+// Try running in the console below.
+  
+exports = function(payload, response) {
+    var conn = context.services.get("mongodb-atlas").db("bowling").collection("games");
+    var obj = JSON.parse(payload.query.obj);
+    obj.played_on = new Date(obj.played_on);
+    conn.insertOne(obj);
+    
+    var html="";
+    
+    html = "Created Record.";
+    if(html.length > 1) {
+      response.setHeader("Content-Type", "text/html");
+      response.setBody(html);
+    }
+  };
