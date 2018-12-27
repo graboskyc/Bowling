0c3db4a268375d830acdf0f782ef96b82b923a76
Chris Grabosky <chris@grabosky.net>
2018-12-27 14:17:35 -0500

    fix bug on spares picked up. changed txt msg to include 30 day avg

--- stitch/functions/recalcStats/source.js
+++ stitch/functions/recalcStats/source.js
@@ -4,6 +4,9 @@ exports = async function(changeEvent) {
   var gconn = context.services.get("mongodb-atlas").db("bowling").collection("games");
   
   var aqr = await gconn.aggregate([{$match:{name:doc.name}},{$group:{_id:"$name",avgpins: { $avg: "$total"}, totpins: { $sum: "$total"}, minpins: {$min: "$total"}, maxpins: {$max: "$total"}}}]).toArray();
+  var lm = new Date();
+  lm.setDate(lm.getDate() - 30);
+  var aqrth = await gconn.aggregate([{$match:{name:doc.name,played_on: {"$gt": lm }}},{$group:{_id:"$name",avgpins: { $avg: "$total"}, totpins: { $sum: "$total"}, minpins: {$min: "$total"}, maxpins: {$max: "$total"}}}]).toArray();
   var gp = await gconn.count({name:doc.name})*1;
   var league = await gconn.aggregate([{$group:{_id:"$name",avgpins: { $avg: "$total"}, totpins: { $sum: "$total"}}}]).toArray();
 
@@ -15,7 +18,7 @@ exports = async function(changeEvent) {
   var st = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$match: {$or: [{'frames.b1':10},{'frames.b2':10},{'frames.b3':10}]}},{$count:"val"}]).toArray();
   var sp = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$match: {$or: [{'frames.spare':true},{'frames.b3spare':true}]}},{$count:"val"}]).toArray();
   var sl = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$match: {'frames.split':true}},{$count:"val"}]).toArray();
-  var spu = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$match: {$and:[ {'frame.split':true},{'frames.b2':'/'}]}},{$count:"val"}]).toArray();
+  var spu = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$match: {$and:[ {'frames.split':true},{'frames.spare':true}]}},{$count:"val"}]).toArray();
   var fba = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$group:{_id:"$name",val:{$avg: "$frames.b1"}}}]).toArray();
   var ofa = await gconn.aggregate([{$match: { name: doc.name}},{$unwind: '$frames'},{$match: {$and: [{'frames.spare':false},{'frames.b1':{$lt: 10}}]}},{$count:"val"}]).toArray();
   var spuct,stct,spct,slct,fbact,ofct = 0;
@@ -76,7 +79,7 @@ exports = async function(changeEvent) {
     twilio.send({
       from: ourNumber,
       to: user.phone,
-      body: 'You bowled a ' + doc.total + ' bringing avg to ' + Math.floor(aqr[0].avgpins) + '. Visit https://grabosky.azurewebsites.net/Bowling/#!' + doc.name
+      body: 'You bowled a ' + doc.total + '. All time avg is ' + Math.floor(aqr[0].avgpins) + ' and 30 day avg is ' + Math.floor(aqrth[0].avgpins) + '. See https://grabosky.azurewebsites.net/Bowling/#!' + doc.name
     });
   }
 };
